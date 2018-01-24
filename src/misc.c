129e4204b74b7fc22d4763352c4bd2f25ebda8f3
mi-scholz <mi-scholz@356274cd-356a-4eb9-bd40-2da9e236b2b7>
2018-01-24 23:32:42 +0000

    bugfix

--- src/misc.c
+++ src/misc.c
@@ -25,10 +25,10 @@
  */
 
 #if !defined(lint)
-const char libfth_sccsid[] = "@(#)misc.c	2.16 1/23/18";
+const char libfth_sccsid[] = "@(#)misc.c	2.17 1/25/18";
 #endif /* not lint */
 
-#define FTH_DATE		"2018/01/23"
+#define FTH_DATE		"2018/01/25"
 
 #if defined(HAVE_CONFIG_H)
 #include "config.h"
