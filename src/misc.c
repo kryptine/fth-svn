817852be9d4b5785dec54af855f8218a0567683f
mi-scholz <mi-scholz@356274cd-356a-4eb9-bd40-2da9e236b2b7>
2018-01-25 13:17:33 +0000

    bugfix

--- src/misc.c
+++ src/misc.c
@@ -25,7 +25,7 @@
  */
 
 #if !defined(lint)
-const char libfth_sccsid[] = "@(#)misc.c	2.17 1/25/18";
+const char libfth_sccsid[] = "@(#)misc.c	2.18 1/25/18";
 #endif /* not lint */
 
 #define FTH_DATE		"2018/01/25"
