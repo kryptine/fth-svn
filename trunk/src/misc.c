96c12c1fed414d91fad978c0236833b23926d168
mi-scholz <mi-scholz@356274cd-356a-4eb9-bd40-2da9e236b2b7>
2020-10-07 01:12:05 +0000

    bugfix

--- trunk/src/misc.c
+++ trunk/src/misc.c
@@ -25,10 +25,10 @@
  */
 
 #if !defined(lint)
-const char libfth_sccsid[] = "@(#)misc.c	3.9 10/6/20";
+const char libfth_sccsid[] = "@(#)misc.c	3.10 10/7/20";
 #endif /* not lint */
 
-#define FTH_DATE		"2020/10/06"
+#define FTH_DATE		"2020/10/07"
 
 #if defined(HAVE_CONFIG_H)
 #include "config.h"
